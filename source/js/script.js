3d82b14dc89a59f3f310ab371f5b7b371e7613cf
yulexmirka <yulexmirka@gmail.com>
2022-08-28 00:57:21 +0300

    fix styles for main-nav

--- source/js/script.js
+++ source/js/script.js
@@ -2,15 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
   let navMain = document.querySelector('.main-nav');
   let navToggle = document.querySelector('.main-nav__toggle');
 
-  navMain.classList.remove('main-nav--nojs');
+  navMain.classList.remove('maim-nav--nojs');
 
   navToggle.addEventListener('click', function () {
-    if (navMain.classList.contains('main-nav--closed')) {
-      navMain.classList.remove('main-nav--closed');
-      navMain.classList.add('main-nav--opened');
-    } else {
-      navMain.classList.add('main-nav--closed');
+    if (navMain.classList.contains('main-nav--opened')) {
       navMain.classList.remove('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--opened');
     }
   });
 });
