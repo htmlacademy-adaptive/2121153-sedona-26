cf911ea2c2ebcba1a2ff04e68d1a4b9cda51bb81
yulexmirka <yulexmirka@gmail.com>
2022-08-28 12:06:10 +0300

    fix js code

--- source/js/script.js
+++ source/js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   let navMain = document.querySelector('.main-nav');
   let navToggle = document.querySelector('.main-nav__toggle');
 
-  navMain.classList.remove('maim-nav--nojs');
+  navMain.classList.remove('main-nav--nojs');
 
   navToggle.addEventListener('click', function () {
     if (navMain.classList.contains('main-nav--opened')) {
